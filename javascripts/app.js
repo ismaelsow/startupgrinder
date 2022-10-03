066e68f14c77facd8eb9301959c63959b31f072b
Ismaël Sow <ismael@searchness.com>
2022-10-03 23:03:02 +0300

    Switch from Wistia to Youtube

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,16 +1,8 @@
 function loadVideo(videoId) {
-  let scriptOne = document.createElement('script');
-  scriptOne.src = `https://fast.wistia.com/embed/medias/${videoId}.jsonp`;
-  scriptOne.sync = true;
-  document.head.appendChild(scriptOne)
-  scriptOne.onload = function () {
-    let scriptTwo = document.createElement('script');
-    scriptTwo.src = `https://fast.wistia.com/assets/external/E-v1.js`;
-    scriptTwo.sync = true;
-    document.head.appendChild(scriptTwo)
-    let videoHTML = `<div class="wistia_responsive_padding" style="padding:75.0% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><div class="wistia_embed wistia_async_${videoId} videoFoam=true" style="height:100%;position:relative;width:100%"><div class="wistia_swatch" style="height:100%;left:0;opacity:0;overflow:hidden;position:absolute;top:0;transition:opacity 200ms;width:100%;"><img src="https://fast.wistia.com/embed/medias/${videoId}/swatch" style="filter:blur(5px);height:100%;object-fit:contain;width:100%;" alt="" aria-hidden="true" onload="this.parentNode.style.opacity=1;" /></div></div></div></div>`;
-    document.querySelector("#failure").innerHTML = videoHTML;
-  };
+  let width = document.querySelector("#branding").clientWidth;
+  let height = Math.round(width * 0.5625); // 0.5625 default ratio
+  let videoHTML = `<iframe width="${width}" height="${height}" src="https://www.youtube.com/embed/${videoId}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+  document.querySelector("#failure").innerHTML = videoHTML;
 }
 
 

e3faec8ac05e0a268e068fb6738b394c28acc5fb
Ismaël Sow <ismael@searchness.com>
2022-10-03 20:11:54 +0300

    Add 'try it' level

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,6 +1,16 @@
 function loadVideo(videoId) {
-  let videoHTML = `<script src="https://fast.wistia.com/embed/medias/471mxiaaxf.jsonp" async></script><script src="https://fast.wistia.com/assets/external/E-v1.js" async></script><div class="wistia_responsive_padding" style="padding:75.0% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><div class="wistia_embed wistia_async_471mxiaaxf videoFoam=true" style="height:100%;position:relative;width:100%"><div class="wistia_swatch" style="height:100%;left:0;opacity:0;overflow:hidden;position:absolute;top:0;transition:opacity 200ms;width:100%;"><img src="https://fast.wistia.com/embed/medias/${videoId}/swatch" style="filter:blur(5px);height:100%;object-fit:contain;width:100%;" alt="" aria-hidden="true" onload="this.parentNode.style.opacity=1;" /></div></div></div></div>`;
-  document.querySelector("#failure").innerHTML = videoHTML;
+  let scriptOne = document.createElement('script');
+  scriptOne.src = `https://fast.wistia.com/embed/medias/${videoId}.jsonp`;
+  scriptOne.sync = true;
+  document.head.appendChild(scriptOne)
+  scriptOne.onload = function () {
+    let scriptTwo = document.createElement('script');
+    scriptTwo.src = `https://fast.wistia.com/assets/external/E-v1.js`;
+    scriptTwo.sync = true;
+    document.head.appendChild(scriptTwo)
+    let videoHTML = `<div class="wistia_responsive_padding" style="padding:75.0% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><div class="wistia_embed wistia_async_${videoId} videoFoam=true" style="height:100%;position:relative;width:100%"><div class="wistia_swatch" style="height:100%;left:0;opacity:0;overflow:hidden;position:absolute;top:0;transition:opacity 200ms;width:100%;"><img src="https://fast.wistia.com/embed/medias/${videoId}/swatch" style="filter:blur(5px);height:100%;object-fit:contain;width:100%;" alt="" aria-hidden="true" onload="this.parentNode.style.opacity=1;" /></div></div></div></div>`;
+    document.querySelector("#failure").innerHTML = videoHTML;
+  };
 }
 
 
@@ -92,12 +102,27 @@ function totalScore(){
 
 
 function handleSuccess(score) {
-  document.querySelector("#subtitle").innerHTML = `Bootstrapping score: <span class="bg-gradient" style="display:inline-block;border-radius:8px;font-weight:bold;background-color:rgb(0, 188, 135);padding:8px">${score}/10&nbsp;&nbsp;TRY IT!</span>`
+  document.querySelector("#subtitle").innerHTML = `Bootstrapping score: <span class="bg-gradient" style="display:inline-block;border-radius:8px;font-weight:bold;background-color:rgb(0, 188, 135);padding:8px">${score}/10&nbsp;&nbsp;DO IT!</span>`
   document.querySelector("#subtitle").style.paddingBottom = "12px"
   document.querySelector("#success-picture").setAttribute("src", window.wins[Math.round(Math.random() * (window.wins.length - 1))]);
   document.querySelector("#wait-animation").style.display = "block";
   setTimeout(() => {
     var audio = new Audio('audio/success.mp3');
+    audio.play();
+    document.querySelector("#wait-animation").style.display = "none";
+    document.querySelector("#success").style.display = "block";
+    document.querySelector("#follow-on-twitter").style.display = "block";
+  }, 1500);
+}
+
+
+function handleMiddle(score) {
+  document.querySelector("#subtitle").innerHTML = `Bootstrapping score: <span class="bg-warning bg-gradient" style="display:inline-block;border-radius:8px;font-weight:bold;background-color:rgb(0, 188, 135);padding:8px">${score}/10&nbsp;&nbsp;TRY IT?</span>`
+  document.querySelector("#subtitle").style.paddingBottom = "12px"
+  document.querySelector("#success-picture").setAttribute("src", window.fines[Math.round(Math.random() * (window.fines.length - 1))]);
+  document.querySelector("#wait-animation").style.display = "block";
+  setTimeout(() => {
+    var audio = new Audio('audio/middle.mp3');
     audio.play();
     document.querySelector("#wait-animation").style.display = "none";
     document.querySelector("#success").style.display = "block";
@@ -135,6 +160,9 @@ document.querySelector("#summon-the-grinder").addEventListener("click", function
 
   if (score >= 8) {
     handleSuccess(score);
+  }
+  else if (score >= 6) {
+    handleMiddle(score);
   }
   else {
     handleFailure(score);

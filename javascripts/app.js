f8ddac3dfee331fe76ad4d3bb1836376969540c9
Ismaël Sow <ismael@searchness.com>
2022-10-03 17:57:43 +0300

    Fix some stuff on mobile

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,3 +1,27 @@
+function getOS() {
+  var userAgent = window.navigator.userAgent,
+    platform = window.navigator?.userAgentData?.platform || window.navigator.platform,
+    macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K', 'macOS'],
+    windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+    iosPlatforms = ['iPhone', 'iPad', 'iPod'],
+    os = null;
+
+  if (macosPlatforms.indexOf(platform) !== -1) {
+    os = 'Mac OS';
+  } else if (iosPlatforms.indexOf(platform) !== -1) {
+    os = 'iOS';
+  } else if (windowsPlatforms.indexOf(platform) !== -1) {
+    os = 'Windows';
+  } else if (/Android/.test(userAgent)) {
+    os = 'Android';
+  } else if (/Linux/.test(platform)) {
+    os = 'Linux';
+  }
+
+  return os;
+}
+
+
 function isFormValid() {
   valid = true;
   if (!document.querySelector("#problem").value){
